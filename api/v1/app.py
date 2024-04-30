f87be0a76512e0920c8447662e2d985434128ed0
Ladydee3 <delisiletwala62@gmail.com>
2024-04-30 14:29:27 -0700

    task3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,45 @@
-from flask import Flask
+#!/usr/bin/python3
+"""
+app
+"""
+
+from flask import Flask, jsonify
+from flask_cors import CORS
+from os import getenv
+
 from api.v1.views import app_views
 from models import storage
 
+
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
+
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
-def teardown_db(exception):
+def teardown(exception):
+    """
+    teardown function
+    """
     storage.close()
 
-if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
 
+@app.errorhandler(404)
+def handle_404(exception):
+    """
+    handles 404 error
+    :return: returns 404 json
+    """
+    data = {
+        "error": "Not found"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 404
+
+    return(resp)
+
+if __name__ == "__main__":
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,46 @@
-from api.v1.views import app_views
+#!/usr/bin/python3
+"""
+index
+"""
+
 from flask import jsonify
+from api.v1.views import app_views
 
-@app_views.route('/status', methods=['GET'])
-def api_status():
-    return jsonify({"status": "OK"})
+from models import storage
 
+
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
+def status():
+    """
+    status route
+    :return: response with json
+    """
+    data = {
+        "status": "OK"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    stats of all objs route
+    :return: json of all objs
+    """
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
