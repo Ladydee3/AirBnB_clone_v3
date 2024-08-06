abe55cea199132351337e08f7044720163b63d82
Ladydee3 <delisilewala62@gmail.com>
2024-08-06 13:14:15 -0700

    changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python3
-"""Connect to API"""
-import os
+"""flask"""
+from flask import Flask, render_template, jsonify
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify, make_response
+from os import getenv
 from flask_cors import CORS
 
-
 app = Flask(__name__)
-# app.register_blueprint(app_views, url_prefix="/api/v1")
-app.register_blueprint(app_views)
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+app.register_blueprint(app_views, url_prefix="/api/v1")
+host = getenv('HBNB_API_HOST') if getenv('HBNB_API_HOST') else '0.0.0.0'
+port = getenv('HBNB_API_PORT') if getenv('HBNB_API_PORT') else 5000
+cors = CORS(app, resources={'/*': {'origins': '0.0.0.0'}})
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """Closes the storage."""
+def teardown_db(self):
+    """teardown"""
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """Handler for 404 errors, returns a JSON-formatted response."""
-    return make_response(jsonify({'error': 'Not found'}), 404)
+def page_not_found(e):
+    """404ed"""
+    return jsonify({"error": "Not found"}), 404
 
-
-if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+if __name__ == '__main__':
+    app.run(host=host, port=port, threaded=True)app.py
